dac31a4f5b8c1145c86785d2ceb9db8e76ca3e45
Joan Sánchez <joansvich@gmail.com>
2019-03-08 14:57:35 +0100

    Added update

--- routes/index.js
+++ routes/index.js
@@ -13,9 +13,37 @@ router.get('/', async (req, res, next) => {
   }
 });
 
-router.get('/profile', async (req, res, next) => {
+router.get('/profile/:id', async (req, res, next) => {
+  const { id } = req.params;
   try {
-    res.render('profile');
+    const user = await User.findById(id);
+    res.render('profile', user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/profile/:id/edit', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    console.log(user);
+    res.render('edit-profile', user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/profile/update', async (req, res, next) => {
+  const { username, description } = req.body;
+  const currentUser = req.session.currentUser;
+  try {
+    const editUser = {
+      username,
+      description
+    };
+    await User.findOneAndUpdate(currentUser._id, editUser);
+    res.redirect(`/profile/${currentUser._id}`);
   } catch (error) {
     next(error);
   }
