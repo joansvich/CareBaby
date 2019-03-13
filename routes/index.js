5497b1c3f02ca966a513fdea1a56a255404ac514
Joan Sánchez <joansvich@gmail.com>
2019-03-13 10:17:33 +0100

    Added rating

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,13 @@ var router = express.Router();
 router.get('/', async (req, res, next) => {
   try {
     const babySitterArray = await User.find({ userType: 'babysitter' });
+    babySitterArray.forEach((babysitter) => {
+      if (babysitter.totalFeedback === 0) {
+        babysitter.rating = 0;
+      } else {
+        babysitter.rating = (babysitter.positiveFeedback / babysitter.totalFeedback * 10).toFixed();
+      }
+    });
     res.render('home', { babySitterArray });
   } catch (error) {
     next(error);
@@ -142,6 +149,11 @@ router.get('/profile/:id', userIsNotLogged, async (req, res, next) => {
   let isBabySitter = false;
   try {
     const userSelected = await User.findById(id);
+    if (userSelected.totalFeedback === 0) {
+      userSelected.rating = 0;
+    } else {
+      userSelected.rating = ((userSelected.positiveFeedback / userSelected.totalFeedback) * 10).toFixed();
+    }
     if (id === currentUser._id) {
       console.log(currentUser.userType);
       isMyUser = true;
