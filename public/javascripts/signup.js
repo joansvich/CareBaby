9fa67e2a864dfd95a5828416bdd53a33b42d796f
Joan Sánchez <joansvich@gmail.com>
2019-03-13 10:30:06 +0100

    Update signup

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -6,7 +6,7 @@ const main = () => {
       const inputLocation = document.querySelector('.input-location');
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
-      let loc = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?types=region&limit=1&access_token=pk.eyJ1IjoiY2FyZWJhYnkiLCJhIjoiY2p0MWNrOHplMHBhdTRibXh3aXRsNDk4cCJ9.zeNJPvVOdrinJmH7mxLO3w`);
+      let loc = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?types=place&limit=1&access_token=pk.eyJ1IjoiY2FyZWJhYnkiLCJhIjoiY2p0MWNrOHplMHBhdTRibXh3aXRsNDk4cCJ9.zeNJPvVOdrinJmH7mxLO3w`);
       const arrayLoc = await loc.json();
       inputLocation.value = arrayLoc.features[0].place_name;
 
